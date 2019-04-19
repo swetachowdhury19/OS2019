ce48932043be05022f96a3df79ef62c790830b71
swetachowdhury19 <49769697+swetachowdhury19@users.noreply.github.com>
2019-04-19 23:47:11 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -38,3 +38,18 @@ void Process:: input(int arraival_time,int Brust_time)
 		p[1].tat=p[1].completion_time-p[1].arraival_time;
 		p[2].tat=p[2].completion_time-p[2].arraival_time;
 		p[3].tat=p[3].completion_time-p[3].arraival_time;
+
+		
+				//*******  WAITING TIME CALCULATION     ******
+		
+		p[0].waiting_time=p[0].tat-p[0].Brust_time;
+		p[1].waiting_time=p[1].tat-p[1].Brust_time;
+		p[2].waiting_time=p[2].tat-p[2].Brust_time;
+		p[3].waiting_time=p[3].tat-p[3].Brust_time;
+		
+		//*******  PRIORITY  CALCULATION      *******
+		
+		p[0].priority=1+(p[0].waiting_time/p[0].Brust_time);
+		p[1].priority=1+(p[1].waiting_time/p[1].Brust_time);
+		p[2].priority=1+(p[2].waiting_time/p[2].Brust_time);
+		p[3].priority=1+(p[3].waiting_time/p[3].Brust_time);
