2decf4fd49ab6d695d35d2e5f60b7c18335f737a
swetachowdhury19 <49769697+swetachowdhury19@users.noreply.github.com>
2019-04-19 23:43:45 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -18,3 +18,23 @@ class Process{
 	void static p_time(Process []);
 			 
 };
+void Process:: input(int arraival_time,int Brust_time)
+	{
+		this->Brust_time=Brust_time;
+		this->arraival_time=arraival_time;
+	}
+	void Process:: c_time(Process p[])
+	{
+		
+		//*******  COMPLETION TIME CALCULATION ******
+		p[0].completion_time=p[0].Brust_time;
+		p[1].completion_time=p[0].completion_time+p[1].Brust_time;
+		p[2].completion_time=p[1].completion_time+p[2].Brust_time;
+		p[3].completion_time=p[2].completion_time+p[3].Brust_time;
+		
+		//*******  TURNAROUND TIME CALCULATION ******
+		
+		p[0].tat=p[0].completion_time-p[0].arraival_time;
+		p[1].tat=p[1].completion_time-p[1].arraival_time;
+		p[2].tat=p[2].completion_time-p[2].arraival_time;
+		p[3].tat=p[3].completion_time-p[3].arraival_time;
